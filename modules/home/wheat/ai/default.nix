3ef4a97f71d5978cdd767f6ee26e94b829370051
PeteE <pete.perickson@gmail.com>
2025-07-20 23:40:46 -0500

    chore: wip

--- modules/home/wheat/ai/default.nix
+++ modules/home/wheat/ai/default.nix
@@ -14,9 +14,9 @@ in {
     home.packages = with pkgs; [
       claude-code
     ];
-    sops.secrets."aichat" = {
-      path = "${config.home.homeDirectory}/.config/aichat/config.yaml";
-    };
+    # sops.secrets."aichat" = {
+    #   path = "${config.home.homeDirectory}/.config/aichat/config.yaml";
+    # };
     programs.aichat = {
       enable = true;
     };
@@ -25,11 +25,27 @@ in {
     sops.secrets.anthropicApiKey = { };
     sops.secrets.assemblyAiApiKey = { };
     sops.secrets.opaqueGithubToken = { };
-    programs.zsh.envExtra = ''
-      export OPENAI_API_KEY=$(cat ${config.sops.secrets.openaiApiKey.path})
-      export ANTHROPIC_API_KEY=$(cat ${config.sops.secrets.anthropicApiKey.path})
-      export ASSEMBLYAI_API_KEY=$(cat ${config.sops.secrets.assemblyAiApiKey.path})
-      export OPAQUE_GITHUB_TOKEN=$(cat ${config.sops.secrets.opaqueGithubToken.path})
+    programs.zsh = {
+      envExtra = ''
+        export OPENAI_API_KEY=$(cat ${config.sops.secrets.openaiApiKey.path})
+        export ANTHROPIC_API_KEY=$(cat ${config.sops.secrets.anthropicApiKey.path})
+        export ASSEMBLYAI_API_KEY=$(cat ${config.sops.secrets.assemblyAiApiKey.path})
+        export OPAQUE_GITHUB_TOKEN=$(cat ${config.sops.secrets.opaqueGithubToken.path})
       '';
+      completionInit = ''
+        _aichat_zsh() {
+          if [[ -n "$BUFFER" ]]; then
+              local _old=$BUFFER
+              BUFFER+="⌛"
+              zle -I && zle redisplay
+              BUFFER=$(aichat -e "$_old")
+              zle end-of-line
+          fi
+        }
+        zle -N _aichat_zsh
+        wl-copy
+        bindkey '\ee' _aichat_zsh
+      '';
+    };
   };
 }
--- modules/nixos/wheat/plasma/default.nix
+++ modules/nixos/wheat/plasma/default.nix
@@ -32,5 +32,9 @@ in {
     services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      wl-clipboard-rs
+    ];
   };
 }
