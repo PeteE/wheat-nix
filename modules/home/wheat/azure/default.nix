01dde6bf6ca1eca8885deca6aaccec10f0ade9c2
PeteE <pete.perickson@gmail.com>
2025-08-01 22:37:37 -0500

    fix: using azure-cli from stable

--- modules/home/wheat/azure/default.nix
+++ modules/home/wheat/azure/default.nix
@@ -6,17 +6,19 @@
   format, # A normalized name for the home target (eg. `home`).
   virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
   host, # The host name for this home.
+  system,
   config,
   ...
 }:
 with lib; let
   cfg = config.wheat.azure;
+  pkgs-stable = inputs.nixpkgs-stable.legacyPackages."${system}";
 in {
   options.wheat.azure = {
     enable = mkEnableOption "Enable";
   };
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
+    home.packages = with pkgs-stable; [
       azure-cli
     ];
   };
